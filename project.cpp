cbc65a6ceaac4b99d1687e3061005bf4ddfe70d9
CHUVISS <93385776+CHUVISS@users.noreply.github.com>
2022-01-06 14:55:18 +0300

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,38 +1,47 @@
 #include <Your library path>
-void sharic();
-void palochka();
-void martira();
+void sharic(int x, int y);
+void palochka(int xp);
+void martira(int xm);
 int main()
 {
+    int t = 0;
+    while (t < 18)
+    {
+        int x = 0;
+        int y = 0;
+        int xp = 0;
+        int xm = 0;
+        txCreateWindow(500, 450);
+        txSetFillColor(RGB(0, 191, 255));
+        txRectangle(0, 0, 500, 450);
 
-    txCreateWindow(500, 450);
-    txSetFillColor(RGB(0, 191, 255));
-    txRectangle(0, 0, 500, 450);
+        txSetColor(TX_BLACK);
 
-    txSetColor(TX_BLACK);
-
-    palochka();
-    martira();
-    sharic();
+        palochka(xp);
+        martira(xm);
+        sharic(x, y);
+        t++;
+        txSleep(100);
+    };
     return 0;
 };
-void martira()
+void martira(int xm)
 {
     txSetFillColor(RGB(102, 255, 0));
     txRectangle(0, 450, 175, 300);
     txSetFillColor(RGB(27, 30, 35));
-    POINT Wert[4] = {{120, 285}, {130, 250}, {150, 250}, {160,285}};
+    POINT Wert[4] = {{120, 285}, {130+xm, 250}, {150+xm, 250}, {160,285}};
     txPolygon(Wert, 4);
     txSetFillColor(RGB(118, 92, 72));
     txRectangle(110, 300, 170, 285);
 };
-void palochka()
+void palochka(int xp)
 {
     txSetFillColor(RGB(128, 128, 128));
-    txRectangle(90, 310, 94, 280);
+    txRectangle(90+xp, 310, 94+xp, 280);
 };
-void sharic()
+void sharic(int x, int y)
 {
     txSetFillColor(TX_BLACK);
-    txCircle(140, 230, 10);
+    txCircle(140 + x, 230 + y, 10);
 }
